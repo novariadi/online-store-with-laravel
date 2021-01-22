e85764995a5358d01b7089f883e1012c8b5458b0
novariadi <mnovariadi@gmail.com>
2021-01-23 00:55:47 +0700

    push data

---
+++ app/Http/Controllers/Utama.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\M_barang;
+
+class Utama extends Controller
+{
+    public function index(){
+        return view('Utama');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, ['file' => 'required|max:2048']);
+
+        $file = $request->file('file');
+        $nama_file = time() . "_". $file->getClientOriginalName();
+        $tujuan_upload = 'data_file';
+        if($file->move($tujuan_upload, $nama_file)){
+            $data = M_Barang::create([
+                'nama_produk' => $request->nama_produk,
+                'harga' => $request->harga,
+                'gambar' => $nama_file
+            ]);
+            $res['massage'] = 'Success!';
+            $res['value'] = $data;
+            return response($res);
+        }
+    }
+}
